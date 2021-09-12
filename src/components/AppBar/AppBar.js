05455be2942a6c9adf5e34b316d225d7b453d945
Leskovets Maksym <leskovecmaksim@gmail.com>
2021-09-12 20:38:31 +0200

    add bar

---
+++ src/components/AppBar/AppBar.js
@@ -0,0 +1,10 @@
+import Navigation from "../Navigation/Navigation";
+import styles from "./Appbar.module.css";
+
+export default function AppBar() {
+  return (
+    <header className={styles.header}>
+      <Navigation />
+    </header>
+  );
+}
